31613d5bb61077f9b2c16e34ab18cee778bb5674
GrzLnx <53223526+GrzLnx@users.noreply.github.com>
2020-06-22 23:07:58 +0200

    Form update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,24 +3,27 @@
 
 @section( 'content' )
     <main>
+        <section class="grid-parent" id="section--login">
 
-        <div class="textblock textblock--small-margin-bottom">
-            <h1>{{ __('Login') }}</h1>
-        </div>
+            <div class="textblock grid-child">
+                <h1>{{ __('Login') }}</h1>
+            </div>
 
-        {{ $logInForm -> setFormID( 'form-login' ) }}
-        {{ $logInForm -> setFormFieldIDAddPrefix( true ) }}
-        {{ $logInForm -> setFormAction( route( 'login' ) ) }}
-        {{ $logInForm -> addCSRFField( csrf_token() ) }}
-        {{ $logInForm -> addEmailInputField( 'email', '', '', 'email', 'autofocus', old( 'email' ), 'E-mail', 'email', true, __('E-Mail Address'), 'required:true' ) }}
-        {{ $logInForm -> addPasswordInputField( 'password', '', '', 'password', '', '', 'Wachtwoord', 'password', true, __('Password'), 'required:true' ) }}
-        {{ $logInForm -> addCheckboxInputField( 'remember', '', '', 'remember', '', '', true, __('Remember Me'), '' ) }}
-        {{ $logInForm -> addSubmitButton( 'submit', '', '', 'submit', '', __( 'Login' ) ) }}
-        @if( Route::has( 'password.request' ) )
-            {{ $logInForm -> addHTML( '<a href="' . route( 'password.request' ) . '">' . __( 'Forgot your password?' ) . '</a>' ) }}
-        @endif
-        {{ $logInForm -> renderForm() }}
-        {{-- toevoegen error optie --}}
+            {{ $logInForm -> setFormID( 'form-login' ) }}
+            {{ $logInForm -> setFormAdditionalClasses( 'grid-child' ) }}
+            {{ $logInForm -> setFormFieldIDAddPrefix( true ) }}
+            {{ $logInForm -> setFormAction( route( 'login' ) ) }}
+            {{ $logInForm -> addCSRFField( csrf_token() ) }}
+            {{ $logInForm -> addEmailInputField( 'email', '', '', 'email', 'autofocus', old( 'email' ), 'E-mail', 'email', true, __('E-Mail Address'), 'required:true' ) }}
+            {{ $logInForm -> addPasswordInputField( 'password', '', '', 'password', '', '', 'Wachtwoord', 'password', true, __('Password'), 'required:true' ) }}
+            {{ $logInForm -> addCheckboxInputField( 'remember', '', '', 'remember', '', '', true, __('Remember Me'), '' ) }}
+            {{ $logInForm -> addSubmitButton( 'submit', '', '', 'submit', '', __( 'Login' ) ) }}
+            @if( Route::has( 'password.request' ) )
+                {{ $logInForm -> addHTML( '<a href="' . route( 'password.request' ) . '">' . __( 'Forgot your password?' ) . '</a>' ) }}
+            @endif
+            {{ $logInForm -> renderForm() }}
+            {{-- toevoegen error optie --}}
 
+        </section>
     </main>
 @endsection
